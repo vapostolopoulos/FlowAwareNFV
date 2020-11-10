0f24c3b620735db1ca01ac11211f4597ec2d31d4
el10686 <el10686@mail.ntua.gr>
2020-11-10 18:35:20 +0000

    Modify auto vpp launch

---
+++ experiments/hera/launch_vpps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Launch vpprouters
+for (( i=1; i<3; i++))
+do
+	echo --------------------------------------------router$i----------------------------------------------------------------
+	./vpprouter$i.conf &
+	BACK_PID=$!
+	wait $BACK_PID
+	sleep 1
+	echo
+done
+
+#Launch vpps
+for (( i=1; i<16; i++))
+do
+	echo -----------------------------------------------vpp$i----------------------------------------------------------------
+	./vpp$i.conf &
+	BACK_PID=$!
+	wait $BACK_PID
+	sleep 1
+	echo
+done
