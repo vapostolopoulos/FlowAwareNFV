dac805efe9f041a21c9c91376a8f698da066cbb3
el10686 <el10686@mail.ntua.gr>
2020-11-14 18:20:06 +0000

    Try alt memif sockets

--- vpp_scheduler/cleanup.sh
+++ vpp_scheduler/cleanup.sh
@@ -2,4 +2,4 @@ ps -ef | grep vpp | awk '{print $2}'| xargs sudo kill
 sudo rm -rf /run/vpp/cli-*
 sudo rm -rf /run/vpp/stats-*
 sudo rm -rf /run/vpp/vpp*
-sudo rm /run/vpp/memif{1,2,3,4,5}.sock
+sudo rm /run/vpp/memif{1..17}.sock
